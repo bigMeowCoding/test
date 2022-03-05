078147d448a074398a1c389147a5bd7ac28660a0
zhouyijun <zhou495248579@hotmail.com>
2022-03-05 19:39:12 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
-import BScroll from '@better-scroll/core'
-import eq from 'lodash/eq'
-import {from, Subject} from 'rxjs';
-import {sum} from "./sum";
+import {eq} from 'lodash-es'
 
-let wrapper = document.querySelector('.wrapper')
-let scroll = new BScroll(wrapper)
-
-console.log(eq(1,2),eq('s','s'))
-
-const subject = new Subject();
-
-subject.subscribe({
-    next: (v) => console.log(`observerA: ${v}`)
-});
-subject.subscribe({
-    next: (v) => console.log(`observerB: ${v}`)
-});
-
-const observable = from([1, 2, 3]);
-
-observable.subscribe(subject);
-console.log(sum(1,2))
+console.log(eq(1,2))
