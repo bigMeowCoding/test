2cafb0401a9955b1c87be057edd8b5ccb89ed6cc
zhouyijun <zhou495248579@hotmail.com>
2022-07-25 22:01:09 +0800

    feat:tailwind css

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,6 @@ import React, { useEffect, useRef, useState } from "react";
 import request from "umi-request";
 
 const App = () => {
-  return (
-    <h1>
-      <div>
-        <button onClick={() => {}}>click</button>
-      </div>
-      React App
-    </h1>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 export default App;
