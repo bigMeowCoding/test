e44bde61c0807f6410a427ba3442ab005b1ef5c8
zhouyijun <zhou495248579@hotmail.com>
2023-03-04 15:48:09 +0800

    feat:immer

--- a.js
+++ a.js
@@ -1,1 +1,19 @@
-console.log('3vvvvv22222')
+const baseState = [
+  {
+    title: "Learn TypeScript",
+    done: true,
+  },
+  {
+    title: "Try Immer",
+    done: false,
+  },
+];
+
+import produce from "immer";
+
+const nextState = produce(baseState, (draft) => {
+  draft[1].done = true;
+  draft.push({ title: "Tweet about it" });
+});
+
+console.log(nextState,nextState === baseState);
